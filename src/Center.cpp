3041867b9c932c850668e4b28f65ec4d519f7720
JHud <219741840+JamHudson@users.noreply.github.com>
2026-02-26 19:45:33 +0000

    Refactored Center class out, added ifndef guards

---
+++ src/Center.cpp
@@ -0,0 +1,28 @@
+#include <bn_fixed.h>
+#include <bn_fixed_point.h>
+#include <bn_sprite_items_square.h>
+
+#include "movement.h"
+#include "Center.h"
+
+Center::Center(bn::fixed_point starting_position, bn::fixed mass, bn::fixed speed)
+    : _sprite(bn::sprite_items::square.create_sprite(starting_position)),
+      _mass(mass),
+      _speed(speed)
+{
+}
+
+void Center::update()
+{
+    dPadMoveSprite(_sprite, _speed);
+}
+
+bn::fixed_point Center::position()
+{
+    return _sprite.position();
+}
+
+bn::fixed Center::mass()
+{
+    return _mass;
+}
